988212d4db2d2868b53bad75b2d4e9ceba435c9b
ByteBigBoss <nethminas42@gmail.com>
2024-05-22 10:16:27 +0530

    re order files

---
+++ src/icx/menu/MenuEvent.java
@@ -0,0 +1,10 @@
+package icx.menu;
+
+/**
+ *
+ * @author sandaruwan
+ */
+public interface MenuEvent {
+
+    public void menuSelected(int index, int subIndex, MenuAction action);
+}
---
+++ src/icx/service/UserService.java
@@ -0,0 +1,9 @@
+package icx.service;
+
+/**
+ *
+ * @author sandaruwan
+ */
+public interface UserService {
+    
+}
