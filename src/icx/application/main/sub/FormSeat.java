0045405875386753f2350bc6ca3a0ad6a42d43cc
Prabugs <chathuraprabash4218@gmail.com>
2024-05-22 17:48:10 +0530

    Commit No: 01 Date: 2024-05-22
    Changes: 
    *) Test

--- src/icx/application/main/sub/FormSeat.java
+++ src/icx/application/main/sub/FormSeat.java
@@ -19,9 +19,10 @@ public class FormSeat extends javax.swing.JPanel {
     private void initComponents() {
 
         lb = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
 
         lb.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lb.setText("Seat Management Form");
+        lb.setText("Seat Management");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -29,19 +30,23 @@ public class FormSeat extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 794, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(lb)
+                .addContainerGap(720, Short.MAX_VALUE))
+            .addComponent(jSeparator1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(lb, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(416, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lb;
     // End of variables declaration//GEN-END:variables
 }
