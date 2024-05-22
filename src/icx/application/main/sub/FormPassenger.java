f511cb492698830a73a1c9349e27c4bcb50f5420
Sahan <ks.official.sahan@gmail.com>
2024-05-22 13:37:08 +0530

    commit 01

--- src/icx/application/main/sub/FormPassenger.java
+++ src/icx/application/main/sub/FormPassenger.java
@@ -10,8 +10,8 @@ public class FormPassenger extends javax.swing.JPanel {
 
     public FormPassenger() {
         initComponents();
-        lb.putClientProperty(FlatClientProperties.STYLE, ""
-                + "font:$h1.font");
+//        lb.putClientProperty(FlatClientProperties.STYLE, ""
+//                + "font:$h1.font");
       
     }
 
@@ -19,30 +19,18 @@ public class FormPassenger extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lb = new javax.swing.JLabel();
-
-        lb.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lb.setText("Passenger Management Form");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 794, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 806, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 460, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel lb;
     // End of variables declaration//GEN-END:variables
 }
