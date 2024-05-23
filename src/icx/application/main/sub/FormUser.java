7bd3ecf85770fd46973fc88288df3d536a0ee585
Yasith Sandesh <114634534+yasithsandesh@users.noreply.github.com>
2024-05-23 21:26:59 +0530

    UserService 1.0

--- src/icx/application/main/sub/FormUser.java
+++ src/icx/application/main/sub/FormUser.java
@@ -1,6 +1,9 @@
 package icx.application.main.sub;
 
 import com.formdev.flatlaf.FlatClientProperties;
+import icx.application.main.sub.gui.UserManagement;
+import java.awt.BorderLayout;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -10,19 +13,36 @@ public class FormUser extends javax.swing.JPanel {
 
     public FormUser() {
         initComponents();
-        lb.putClientProperty(FlatClientProperties.STYLE, ""
-                + "font:$h1.font");
-        
+
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lb = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
-        lb.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lb.setText("User Management Form");
+        setPreferredSize(new java.awt.Dimension(806, 460));
+
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jButton1.setText("Manage Users");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, java.awt.BorderLayout.CENTER);
+
+        jButton2.setText("Manage Users");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, java.awt.BorderLayout.CENTER);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -30,19 +50,36 @@ public class FormUser extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 794, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+
+        UserManagement userManagemnet = new UserManagement();
+        jPanel1.removeAll();
+        jPanel1.add(userManagemnet, BorderLayout.CENTER);
+        SwingUtilities.updateComponentTreeUI(jPanel1);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel lb;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/icx/service/UserService.java
+++ src/icx/service/UserService.java
@@ -1,9 +1,35 @@
 package icx.service;
 
+import icx.model.UserLoginReturnDTO;
+import java.util.HashMap;
+import java.util.Vector;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+
+
+
 /**
  *
- * @author sandaruwan
+ * @author yasith sandesh
  */
 public interface UserService {
+    
+    public UserLoginReturnDTO userLogin(String email,String password);
+    
+    public void userAdd(String mobile,String fname,String lname,String password,String email,String userType);
+    
+    public HashMap<String,String> loadUserTypes(JComboBox jcombobox);
+    
+    public void loadUsers(JTable jTable,String query);
+    
+    public void userUpdate(String u_id,String mobile,String fname,String lname,String password,String email,String status);
+    
+    public void userStatusChange(String u_id,String status);
+    
+    public void addUserRole(String type);
+    
+    public void updateUserRole(String id,String type);
+    
+    public void tableLoadRole(JTable jTable);
     
 }
