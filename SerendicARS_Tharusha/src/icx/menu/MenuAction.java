51ec6549e0bd011cbe2b8f43a386ce68aca5f254
Tharusha Adithya <124893155+tharushx@users.noreply.github.com>
2024-05-23 23:18:52 +0530

    commit

---
+++ SerendicARS_Tharusha/src/icx/menu/MenuAction.java
@@ -0,0 +1,18 @@
+package icx.menu;
+
+/**
+ *
+ * @author sandaruwan
+ */
+public class MenuAction {
+
+    protected boolean isCancel() {
+        return cancel;
+    }
+
+    public void cancel() {
+        this.cancel = true;
+    }
+
+    private boolean cancel = false;
+}
