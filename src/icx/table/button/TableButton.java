75460cde8eac0da5f4727afdc8b5f057b100dfb5
Sahan <ks.official.sahan@gmail.com>
2024-06-09 23:02:09 +0530

    refactored

--- src/icx/table/button/TableButton.java
+++ src/icx/table/button/TableButton.java
@@ -1,9 +1,13 @@
 package icx.table.button;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 
@@ -36,24 +40,24 @@ public class TableButton extends JButton {
         });
     }
 
-//    @Override
-//    protected void paintComponent(Graphics grphcs) {
-//        Graphics2D g2 = (Graphics2D) grphcs.create();
-//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//        int width = getWidth();
-//        int height = getHeight();
-//        int size = Math.min(width, height);
-//        int x = (width - size) / 6;
-//        int y = (height - size) / 2;
-//        if (mousePress) {
-//            g2.setColor(new Color(158, 158, 158));
-//        } else {
-//            g2.setColor(new Color(78, 80, 82));
-//        }
-//        g2.fill(new Rectangle2D.Double(x, y, 100, height));
-////        g2.fill(new Ellipse2D.Double(x, y, 100, height));
-//        g2.dispose();
-//        super.paintComponent(grphcs);
-//    }
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int width = getWidth();
+        int height = getHeight();
+        int size = Math.min(width, height);
+        int x = (width - size) / 6;
+        int y = (height - size) / 2;
+        if (mousePress) {
+            g2.setColor(new Color(158, 158, 158));
+        } else {
+            g2.setColor(new Color(78, 80, 82));
+        }
+        g2.fill(new Rectangle2D.Double(x, y, 100, height));
+//        g2.fill(new Ellipse2D.Double(x, y, 100, height));
+        g2.dispose();
+        super.paintComponent(grphcs);
+    }
 
 }
